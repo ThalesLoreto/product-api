20086b12b6d4448c1d6c07fa64fe613629587d42
ThalesLoreto <tloreto.dev@gmail.com>
2024-07-15 18:10:15 -0300

    feat: create product_handlers & chi routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/ThalesLoreto/product-api/configs"
+	"github.com/ThalesLoreto/product-api/internal/entity"
+	"github.com/ThalesLoreto/product-api/internal/infra/database"
+	"github.com/ThalesLoreto/product-api/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	cfg, _ := configs.LoadConfig(".")
-	fmt.Println(cfg.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+
+	http.ListenAndServe(":3000", r)
 }
---
+++ internal/infra/webserver/handlers/product_handlers.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/ThalesLoreto/product-api/internal/dto"
+	"github.com/ThalesLoreto/product-api/internal/entity"
+	"github.com/ThalesLoreto/product-api/internal/infra/database"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (ph *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = ph.ProductDB.Create(p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
