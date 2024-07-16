afe0d35db2a812c194ec3debc190a1d34ac91228
ThalesLoreto <tloreto.dev@gmail.com>
2024-07-16 02:34:59 -0300

    feat: added userHandler and Create user method

---
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/ThalesLoreto/product-api/internal/dto"
+	"github.com/ThalesLoreto/product-api/internal/entity"
+	"github.com/ThalesLoreto/product-api/internal/infra/database"
+)
+
+type UserHandler struct {
+	UserDB database.UserInterface
+}
+
+func NewUserHandler(db database.UserInterface) *UserHandler {
+	return &UserHandler{
+		UserDB: db,
+	}
+}
+
+func (uh *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = uh.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
